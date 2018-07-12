2227df081b14c7652c32bd9d0dcbebb3a5e46d6a
Yangosoft <yangosoft@gmail.com>
2018-07-12 11:25:51 +0200

    Created develop branch. Added more CMakeLists files

--- exampleclient/src/TConfigMng.cpp
+++ exampleclient/src/TConfigMng.cpp
@@ -29,65 +29,40 @@ TConfigMng::TConfigMng(const std::string& filename)
     Json::Reader r;
     Json::Value val;
 
-    this->m_filename = filename;
-
+    m_filename = filename;
 
 
     std::string s = "{ \"pepe\": \"hola\" }";
     bool result = r.parse(s, val, false);
     std::cout << "SUCCESS? " << result << std::endl;
 
-
-
     Json::Value defaultVal;
-
     std::cout << val.get("pepe", defaultVal).asString() << std::endl;
-
     m_parseCorrect = false;
-
-
 }
 
 bool TConfigMng::readConfiguration()
 {
-
-
     if (false == TFile::fileExists(m_filename))
     {
         return false;
     }
 
     TFile myfile(m_filename);
-
     std::vector<std::string> vLines;
     bool ok = myfile.readByLine(vLines);
 
     if (true == ok)
     {
-
         std::stringstream res;
         std::copy(vLines.begin(), vLines.end(), std::ostream_iterator<std::string>(res, "\n"));
         m_jsonDoc = res.str();
-
-
-
-
-
-
-
-
         std::cout << "Internal rep: " << m_jsonDoc << std::endl;
-
-
-
 
         m_parseCorrect = m_jsonReader.parse(m_jsonDoc, m_jsonRep, false);
         std::cout << "SUCCESS? " << m_parseCorrect << std::endl;
 
-
-
         Json::Value defaultVal;
-
         std::cout << "Puerto: " << m_jsonRep.get("port", defaultVal).asString() << std::endl;
         std::cout << "Broker id: " << m_jsonRep.get("broker_id", defaultVal).asUInt() << std::endl;
     }
@@ -97,7 +72,6 @@ bool TConfigMng::readConfiguration()
 
 bool TConfigMng::getParamValue(const std::string& key, uint32_t& value)
 {
-
     if (false == m_parseCorrect)
     {
         return false;
@@ -111,29 +85,20 @@ bool TConfigMng::getParamValue(const std::string& key, uint32_t& value)
     Json::Value defaultVal;
 
     value = m_jsonRep.get(key, defaultVal).asUInt();
-
-
-
-
-
-
     return false;
-
 }
 
-bool TConfigMng::getParamValue(const std::string& key, std::string& value)
+bool TConfigMng::getParamValue(const std::string& /*key*/, std::string& /*value*/)
 {
     if (false == m_parseCorrect)
     {
         return false;
     }
 
-
-
     return false;
 }
 
-TConfigMng::TConfigMng(const TConfigMng& orig)
+TConfigMng::TConfigMng(const TConfigMng&/* orig*/)
 {
 }
 
